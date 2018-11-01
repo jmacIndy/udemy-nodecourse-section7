f52253b2fbcc0fd8fd2868e83e10119a07261224
Jeff McGinley <jeff.mcginley.indy@gmail.com>
2018-11-01 05:30:51 -0400

    add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,43 @@
+// we won't really use this in our real application
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+   id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+
+// var message = 'I am user #3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// var data = {
+//    id: 4
+// };
+//
+// var token = {
+//    data,
+//    hash: SHA256(JSON.stringify(data) + 'somesecretsaltstring').toString()
+// }
+//
+// // this is how someone would try to break this; howevver, it
+// // will not work because they don't know the secret salt value
+// // which is only stored on the server
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+// // end of how someone would try to break it
+//
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecretsaltstring').toString();
+//
+// if (resultHash === token.hash) {
+//    console.log('data was not changed');
+// } else {
+//    console.log('data was changed - do not trust it');
+// }
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.post('/todos', (req, res) => {
 
    todo.save().then((doc) => {
       res.send(doc);
-   }, (e) => {
+   }).catch((e) => {
       res.status(400).send(e);
    });
 });
@@ -29,8 +29,8 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
    Todo.find().then((todos) => {
       res.send({todos});
-   }, (e) => {
-      res.status(400).send();
+   }).catch((e) => {
+      res.status(400).send(e);
    });
 });
 
@@ -97,6 +97,21 @@ app.patch('/todos/:id', (req, res) => {
 
    }).catch((e) => {
       res.status(400).send();
+   });
+});
+
+app.post('/users', (req, res) => {
+
+   var body = _.pick(req.body, ['email', 'password']);
+
+   var user = new User(body);
+
+   user.save().then(() => {
+      return user.generateAuthToken();
+   }).then((token) => {
+      res.header('x-auth', token).send(user);
+   }).catch((e) => {
+      res.status(400).send(e);
    });
 });
 
